79a717f8f82688cf6858e54caa5a212448fdce29
John Stewart <john.stewart@mongodb.com>
2019-01-16 09:50:01 -0500

    JAVA-3069: fix groovy code violation

--- driver-core/src/test/unit/com/mongodb/internal/connection/DnsMultiServerClusterSpecification.groovy
+++ driver-core/src/test/unit/com/mongodb/internal/connection/DnsMultiServerClusterSpecification.groovy
@@ -17,7 +17,6 @@
 package com.mongodb.internal.connection
 
 import com.mongodb.MongoConfigurationException
-import com.mongodb.MongoSecurityException
 import com.mongodb.ServerAddress
 import com.mongodb.connection.ClusterId
 import com.mongodb.connection.ClusterSettings
