30105c58344bc3b56ea47570adb96514a00efeef
John Stewart <john.stewart@mongodb.com>
2019-10-22 10:01:58 -0400

    JAVA-3243: change reference path for explain

--- driver-legacy/src/main/com/mongodb/DBCursor.java
+++ driver-legacy/src/main/com/mongodb/DBCursor.java
@@ -517,7 +517,7 @@ public class DBCursor implements Cursor, Iterable<DBObject> {
      *
      * @return a {@code DBObject} containing the explain output for this DBCursor's query
      * @throws MongoException if the operation failed
-     * @mongodb.driver.manual reference/explain Explain Output
+     * @mongodb.driver.manual reference/command/explain Explain Output
      * @deprecated Replace with direct use of the explain command using the runCommand helper method
      */
     @Deprecated
